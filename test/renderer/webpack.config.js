413e469f2801f2275d64edc74c600fc4cceb2bb8
Gerald Peel <gerald.p83@outlook.com>
2020-04-22 05:42:01 +0200

    added rendering tests from ol.

---
+++ test/renderer/webpack.config.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const path = require('path')
+
+const cases = path.join(__dirname, 'cases')
+
+const caseDirs = fs.readdirSync(cases).filter((name) => {
+  let exists = true
+  try {
+    fs.accessSync(path.join(cases, name, 'main.js'))
+  } catch (err) {
+    exists = false
+  }
+  return exists
+})
+
+const entry = {}
+caseDirs.forEach((c) => {
+  entry[`cases/${c}/main`] = `./cases/${c}/main.js`
+})
+
+
+module.exports = {
+  context: __dirname,
+  target: 'web',
+  entry: entry,
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  }
+};
